b324b7a242f1d2b46bea0a32c48aa86a474f6fcd
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-01-19 14:52:36 +0100

    Create top-footer section

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,34 @@
 <script>
+import FooterTop from './footer-components/FooterTop.vue';
+
 export default {
     name: 'AppFooter',
+    components: { FooterTop }
 }
 </script>
 
 <template>
+    <footer>
+        <div class="wrapped-container">
 
+            <!-- Import Top Footer Section -->
+            <FooterTop />
+
+            <div class="separator"></div>
+        </div>
+    </footer>
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
 
+footer {
+    padding: 8rem 0 2rem;
+}
+
+div.separator {
+    height: 2px;
+    background-color: $brand-gray;
+    margin: 3rem 0;
+}
 </style>
---
+++ src/components/footer-components/FooterTop.vue
@@ -0,0 +1,52 @@
+<script>
+import { store } from '../../store';
+
+export default {
+    name: 'FooterTop',
+    data() {
+        return {
+            store,
+            footerNavbarList: ['About Us', 'Porfolio', 'Pricing', 'Career', 'Contact']
+        }
+    },
+}
+</script>
+
+<template>
+    <section id="top-footer">
+        <div class="wrapped-container d-flex justify-content-between">
+            <img :src="store.getImagePath('footer-logo')" alt="Anido Logo">
+            <nav class="d-flex">
+                <ul class="m-auto d-flex align-items-center p-0 ">
+                    <li v-for="item in footerNavbarList">
+                        <a href="#">
+                            {{ item }}
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../../styles/partials/variables' as *;
+
+img {
+    width: 180px;
+    object-fit: cover;
+}
+
+nav ul {
+    list-style-type: none;
+
+    li a {
+        margin: 0;
+        margin: 0 1rem;
+        text-decoration: none;
+        color: $black;
+        font-size: 1.5rem;
+        font-weight: 600;
+    }
+}
+</style>
