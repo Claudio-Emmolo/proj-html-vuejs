43f8b38283d995d15f64ef892b7f0168e3647781
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-01-19 10:19:04 +0100

    added contents on main-quick-fact

--- src/components/main-components/MainQuickFact.vue
+++ src/components/main-components/MainQuickFact.vue
@@ -1,78 +1,96 @@
 <script>
+import { store } from '../../store';
+
+import QuickFactCounterCard from './sub-components/QuickFactCounterCard.vue';
+
 export default {
-    name: 'MainQuickFact'
+    name: 'MainQuickFact',
+    components: { QuickFactCounterCard },
+    data() {
+        return {
+            store,
+            counterCardsInfoList: [
+                {
+                    number: '700+',
+                    description: 'Projects done'
+                },
+                {
+                    number: '250+',
+                    description: 'Happy Clients'
+                },
+                {
+                    number: '25+',
+                    description: 'Team Members'
+                },
+                {
+                    number: '12+',
+                    description: 'Years on the market'
+                }
+            ]
+        }
+    },
 }
 </script>
 
 <template>
-    <section id="main-quick-fact">
+    <img :src="store.getImagePath('shape-top')" alt="Shape Wave Top" class="wave-top">
+    <section id="main-quick-fact" class="position-relative">
+        <!-- <img :src="store.getImagePath('globe1')" alt="Globe 1 img"
+            class="globe-bg-img position-absolute top-50 start-50"> -->
         <div class="wrapped-container">
             <h2 class="text-center">Anidio Quick Facts</h2>
+
+            <!-- Import and Generate Counter Cards -->
             <div class="container-fluid">
                 <div class="row justify-content-around">
-
-                    <div class="counter-card col-3 gx43">
-                        <div class="inside-card">
-                            <span>000+</span>
-                            <h3>Project Done</h3>
-                        </div>
-                    </div>
-
-                    <div class="counter-card col-3 gx-4">
-                        <div class="inside-card">
-                            <span>000+</span>
-                            <h3>Project Done</h3>
-                        </div>
-                    </div>
-                    <div class="counter-card col-3 gx-4">
-                        <div class="inside-card">
-                            <span>000+</span>
-                            <h3>Project Done</h3>
-                        </div>
-                    </div>
-                    <div class="counter-card col-3 gx-4">
-                        <div class="inside-card">
-                            <span>000+</span>
-                            <h3>Project Done</h3>
-                        </div>
-                    </div>
+                    <QuickFactCounterCard v-for="counterInfo in counterCardsInfoList" :counterInfo="counterInfo" />
                 </div>
             </div>
+
         </div>
     </section>
+    <img :src="store.getImagePath('shape-bottom')" alt="Shape Wave Top" class="wave-bottom">
 </template>
 
 <style lang="scss" scoped>
 @use '../../styles/partials/variables' as *;
 
+img.wave-top,
+img.wave-bottom {
+    width: 100%;
+    background-image: url(../../assets/preloader.png);
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+img.wave-top {
+    background-position: top;
+
+}
+
+img.wave-bottom {
+    background-position-y: -750px;
+}
+
+img.globe-bg-img {
+    width: 500px;
+    height: 500px;
+    transform: translate(-50%, -50%);
+    opacity: .3;
+}
 
 section#main-quick-fact {
-    background-image: url(../../assets/counter-bg.png);
+    background-image: url(../../assets/preloader.png);
     background-size: cover;
     background-repeat: no-repeat;
     color: $white;
-    padding: 2rem 0;
+    padding: 120px 0;
 }
 
 h2 {
+    z-index: 3;
     font-weight: bold;
     font-size: 4.5rem;
     margin-bottom: 5rem;
-}
-
-div.inside-card {
-    padding: 2rem 4.5rem;
-    text-align: center;
-    border: 1px solid $white;
-    border-radius: 3rem;
-
-    span {
-        font-size: 7rem;
-    }
-
-    h3 {
-        color: $brand-orange;
-        font-size: 1.8rem;
-    }
 }
 </style>
---
+++ src/components/main-components/sub-components/QuickFactCounterCard.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'QuickFactCounterCard',
+    props: ['counterInfo'],
+}
+</script>
+
+<template>
+    <div class="counter-card col-3 gx43">
+        <div class="inside-card">
+            <span>{{ counterInfo.number }}</span>
+            <h3>{{ counterInfo.description }}</h3>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../../../styles/partials/variables' as *;
+
+div.inside-card {
+    padding: 2rem 3.8rem;
+    text-align: center;
+    border: 1px solid $white;
+    border-radius: 3rem;
+
+    span {
+        font-size: 6.8rem;
+        font-weight: 600;
+    }
+
+    h3 {
+        color: $brand-orange;
+        font-size: 1.7rem;
+    }
+}
+</style>
